dc5aa38e772da4913305896f7e40588ee795bf7e
Gianluca Amato <gianluca.amato@unich.it>
2022-09-12 23:05:19 +0200

    Create file wims.con with correct owner and security.

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,8 @@ fi
 if  [ ! -f /home/wims/log/wims.conf ];  then
   echo "threshold1=$(($(cat /proc/cpuinfo | grep processor | wc -l) * 150))" > log/wims.conf
   echo "threshold2=$(($(cat /proc/cpuinfo | grep processor | wc -l) * 300))" >> log/wims.conf
+  chown wims.wims log/wims.conf
+  chmod go-rwx log/wims.conf
 fi
 
 apachectl -D FOREGROUND
