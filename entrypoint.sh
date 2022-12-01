a788f748dbf0a2ed888fddb69de9eda68d770629
Gianluca Amato <gianluca.amato@unich.it>
2022-12-02 00:17:33 +0100

    Wait that reverse proxy is resolvable.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ if [ "$REVERSE_PROXY" != "" ]; then
   echo 'SetEnvIf X-Forwarded-Proto "https" HTTPS=on' > /etc/apache2/conf-enabled/http-proto.conf
   echo 'RemoteIPHeader X-Forwarded-For' >> /etc/apache2/conf-enabled/http-proto.conf
   echo "RemoteIPInternalProxy $REVERSE_PROXY" >> /etc/apache2/conf-enabled/http-proto.conf
+  echo 'Waiting that reverse proxy is resolvable'
+  until getent hosts "$REVERSE_PROXY" > /dev/null; do sleep 5; done
 fi
 
 if [ -f /home/wims/log/logo.jpeg ]; then
